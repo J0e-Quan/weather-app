20bafe98a724ba9f4cd1a33f1fac3ff9f8b06940
Lim Joe Quan <joequanlim@gmail.com>
2026-03-31 18:50:45 +0800

    feat(ui): show current weather and conditions

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,6 @@
 export function showData(data) {
   updateLocation(data.resolvedAddress)
-  updateMainWeather(?, ?) // place current temp and condition here 
+  updateMainWeather(data.currentConditions.feelslike, data.currentConditions.conditions) // place current temp and condition here 
 
 }
 
@@ -12,5 +12,6 @@ function updateLocation(location) {
 function updateMainWeather(temp, condition) {
   const tempText = document.querySelector('.current-temp')
   const conditionText = document.querySelector('.current-condition')
-  tempText.textContent = temp + '°'
-  conditionText.textContent = condition + '°'
+  tempText.textContent = temp + '°' + '  |  '
+  conditionText.textContent = condition.charAt(0).toUpperCase() + condition.slice(1)
+}
