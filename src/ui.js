fefdd0efd0b6be3eb5b9353f4a34171a945a6781
Lim Joe Quan <joequanlim@gmail.com>
2026-03-31 18:59:23 +0800

    fix(ui): make formatting of location consistent
    fix location sometimes appearing as all caps and the first letter of
    words not being capitalised

--- src/ui.js
+++ src/ui.js
@@ -1,12 +1,17 @@
 export function showData(data) {
   updateLocation(data.resolvedAddress)
-  updateMainWeather(data.currentConditions.feelslike, data.currentConditions.conditions) // place current temp and condition here 
+  updateMainWeather(data.currentConditions.feelslike, data.currentConditions.conditions) 
 
 }
 
 function updateLocation(location) {
   const locationText = document.querySelector('.location')
-  locationText.textContent = location.charAt(0).toUpperCase() + location.slice(1)
+  const lowerCasedLocation = location.toLowerCase()
+  // lowerCasedLocation is split with space, each word has its first letter capitalised and is then joined back together
+  const formattedLocation = lowerCasedLocation.split(' ').map(word => { 
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' ')
+  locationText.textContent = formattedLocation.charAt(0).toUpperCase() + formattedLocation.slice(1)
 }
 
 function updateMainWeather(temp, condition) {
