f4be9ede318c9ffedbaf600b66f1d0a340a5043e
Lim Joe Quan <joequanlim@gmail.com>
2026-03-31 16:48:21 +0800

    feat(data) prepare code for mainWeather

--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,16 @@
 export function showData(data) {
   updateLocation(data.resolvedAddress)
-  
+  updateMainWeather(?, ?) // place current temp and condition here 
+
 }
 
 function updateLocation(location) {
   const locationText = document.querySelector('.location')
   locationText.textContent = location.charAt(0).toUpperCase() + location.slice(1)
 }
+
+function updateMainWeather(temp, condition) {
+  const tempText = document.querySelector('.current-temp')
+  const conditionText = document.querySelector('.current-condition')
+  tempText.textContent = temp + '°'
+  conditionText.textContent = condition + '°'
