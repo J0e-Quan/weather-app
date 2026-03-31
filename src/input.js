70248df43db148b16cc30edc6d1eac1719b166bb
Lim Joe Quan <joequanlim@gmail.com>
2026-03-31 21:31:43 +0800

    fix(ui): update data after units are changed

--- src/input.js
+++ src/input.js
@@ -13,15 +13,18 @@ async function handleButtonPress(e) {
   const targetBtn = e.target
   if (targetBtn.classList.contains('search-submit')) {
     const data = await getData(search.value, unitType)
-    console.log(data)
     showData(data)
   } else if (targetBtn.classList.contains('metric')) {
     metric.classList.add('checked')
     imperial.classList.remove('checked')
     unitType = 'metric'
+    const data = await getData(search.value, unitType)
+    showData(data)
   } else if (targetBtn.classList.contains('imperial')) {
     imperial.classList.add('checked')
     metric.classList.remove('checked')
     unitType = 'us'   // visual crossing uses 'us' instead of 'imperial'
+    const data = await getData(search.value, unitType)
+    showData(data)
   }
 }
