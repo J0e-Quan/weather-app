2dd90dc5a6feebc689665dfe8501c1e1b38c40ab
Lim Joe Quan <joequanlim@gmail.com>
2026-03-31 11:44:16 +0800

    feat(data) prepare code for data.js

---
+++ src/data.js
@@ -0,0 +1,3 @@
+export function getData(location) {
+  // query link: https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Penang%20/next7days?unitGroup=metric&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=UWVEYCS26MXWWQV3SSLBXFR8W&contentType=json
+}
