cff392e7ed1bcae69c42b8de86ad8a1515ac5a1f
Lim Joe Quan <joequanlim@gmail.com>
2026-04-04 22:44:56 +0800

    chore(webpack): change WDS port to 3000
    8080 keeps opening via https by default which sends invalid response

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -5,7 +5,8 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    watchFiles: ['./src/index.html']
+    watchFiles: ['./src/index.html'],
+    port: 3000
   },
   module: {
     rules: [
